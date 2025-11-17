2b3d156145b8fdef394cbfec5c17515bf4c3cd5c
dev126712 <solonoobhacker6767@gmail.com>
2025-11-17 13:21:04 -0500

    .

---
+++ bash-notes/data.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+####################
+# Author: Alexandre St-fort
+# Last modified: 11/11/25
+#
+####################
+
+
+filedata=(
+    "data-search.bash"
+)
+
+while IFS=: read -r name id desc; do
+    echo ""
+    echo "name: $name"
+    echo "description: $desc"
+    echo "id: $id"
+    echo ""
+
+
+done < $filedata
---
+++ bash-notes/test.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for arg in "$@"; do
+    count=0
+  # ls -la $arg
+    for files in /$arg/*; do
+        ((count++))
+        echo "$count: $files"
+    done
+done
