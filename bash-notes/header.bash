70cf2cea1c13db695f4479d6261501c16dc4be4f
dev126712 <solonoobhacker6767@gmail.com>
2025-11-20 13:29:41 -0500

    .

--- bash-notes/header.bash
+++ bash-notes/header.bash
@@ -9,7 +9,7 @@ set -x
 awk 'NR==1{print tolower($0)}' people-1000.csv >> list.csv
 
 # take the first line and make evyry character uppercase
-awk 'NR==1{print toupper($0)}' people-1000.csv >> list.csv
+awk 'NR==1{print toupper($0)}' people-1000.csv 
 
 # take the first line and make evyry character lowercase
 awk 'NR==1{print length($0)}' people-1000.csv
@@ -22,10 +22,15 @@ awk '{print length($0)}' people-1000.csv
 sed -zi 's/-/ /g;' people-1000.csv 
 
 # replace every ":" by ","
-sed -zi 's/:/,/g;' people-1000.csv >> list.csv
+sed -zi 's/:/,/g;' people-1000.csv 
 
 # replace every "-" by " "
-sed -zi 's/-/ /g;' people-1000.csv >> list.csv
+sed -zi 's/-/ /g;' people-1000.csv 
 
 # replace every _/ by nothing. \ = escaping
-sed -zi 's/_\///g;' people-1000.csv
+sed -zi 's/_\///g' people-1000.csv
+
+list=$(cat list.csv)
+
+# 1s = only touch the first row .* everything on that row and then replace by the $list
+sed -i "1s/.*/$list/" people-1000.csv
